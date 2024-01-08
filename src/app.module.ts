143ea1bf99f81293e793a130396d75bb1df86e14
Kenys <kenisbohov@gmail.com>
2024-01-08 15:07:45 +0200

    Clear Rep

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.services";
+
 import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: process.env.POST,
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from "./app.module"
 
 async function start() {
     const PORT = process.env.PORT || 7000
+    console.log(PORT);
     const app = await NestFactory.create(AppModule)
 
-    await app.listen(PORT, () => console.log(`Server stared on port = ${{ PORT }}`))
+    await app.listen(PORT, () => console.log(`Server stared on port = ${ PORT }`))
 }
 
 start()

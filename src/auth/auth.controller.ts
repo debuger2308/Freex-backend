7247115f122ffaa65d276902a670d5843c1784d0
Kenys <kenisbohov@gmail.com>
2024-01-09 15:51:32 +0200

    Throttle for Auth/Reg

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,7 @@ export class AuthController {
         return this.authService.login(userDto)
     }
 
+    @Throttle({ default: { ttl: 3600000, limit: 3 } })
     @Post('/registration')
     @UsePipes(ValidationPipe)
     registration(@Body() userDto: CreateUserDto) {
